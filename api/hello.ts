568a2928759caed505183d7c0eb0e18865d37872
kangaroo <hulonghua@Mac-mini.local>
2026-04-20 10:32:53 +0900

    use @vercel/nestjs adapter

--- api/hello.ts
+++ api/hello.ts
@@ -1,45 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from '../src/app.module';
-import serverless from 'serverless-http';
-import express from 'express';
+import { VercelServerlessAdapter } from '@vercel/nestjs';
 
-const app = express();
-const server = express();
-
-server.use(express.json());
-
-// Create NestJS app
-async function createNestApp() {
-  const nestApp = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(server),
-  );
-  
-  nestApp.enableCors({
+export default VercelServerlessAdapter.create(AppModule, {
+  cors: {
     origin: '*',
     credentials: true,
-  });
-  
-  await nestApp.init();
-  return nestApp;
-}
-
-let handler: any;
-
-async function getHandler() {
-  if (!handler) {
-    await createNestApp();
-    handler = serverless(server, {
-      request: (request: any, event: any) => {
-        request.event = event;
-      },
-    });
-  }
-  return handler;
-}
-
-exports.handler = async (event: any, context: any) => {
-  const h = await getHandler();
-  return h(event, context);
-};
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept-Language'],
+  },
+});
